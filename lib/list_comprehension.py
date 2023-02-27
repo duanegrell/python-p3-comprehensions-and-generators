5b13df1c366ecf45918e1179ca4effb24da3e80b
Duane Grell <duanegrell@gmail.com>
2023-02-26 19:08:57 -0500

    passing all tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_elements = [num for num in num_list if num % 2 == 0]
+    return(even_elements)
+    print(even_elements)
+
+# return_evens(num_list=[0, 1, 3, 5, 7, 8, 9, 10])
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed_list = [item + "!" for item in sentence_list]
+    print(exclaimed_list)
+    return(exclaimed_list)
+
+
+# make_exclamation(["Hello", "I'm doing great", "Python is fun"])
